05690fb891b8372d85b17035fca3d5ed8ed6d685
Arjan Malda <95175291+arjanmalda@users.noreply.github.com>
2023-12-27 21:12:58 +0100

    Add uploadx

--- app/page.tsx
+++ app/page.tsx
@@ -63,6 +63,28 @@ export default function Home() {
     checkCamera();
   }, []);
 
+  const handleFileUpload = async (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      try {
+        const photo = await convertToBase64(file);
+        setIsLoading(true);
+        const formData = new FormData();
+        formData.append("photo", file);
+        await fetch("/api/photos", {
+          method: "POST",
+          body: JSON.stringify({ photo }),
+        });
+      } catch (error) {
+        console.error("Error uploading photo: ", error);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+  };
+
   return (
     <main
       className="flex min-h-screen flex-col items-center justify-between p-24 bg-cover bg-center"
@@ -73,10 +95,9 @@ export default function Home() {
           audio={false}
           ref={webcamRef}
           screenshotFormat="image/jpeg"
-          height={720}
+          height={1000}
           videoConstraints={videoConstraints}
           width={1280}
-          screenshotQuality={1080}
           style={{ borderRadius: "2.4rem", boxShadow: "0 0 0 5px #fff" }}
         />
         {isLoading ? (
@@ -84,13 +105,15 @@ export default function Home() {
             <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-blue-400" />
           </div>
         ) : (
-          <button
-            disabled={isLoading}
-            onClick={addPhoto}
-            className="[&_>_svg]:h-14 [&>svg]:w-14 flex justify-center py-4"
-          >
-            <CameraCaptureIcon />
-          </button>
+          <>
+            <button
+              disabled={isLoading}
+              onClick={addPhoto}
+              className="[&_>_svg]:h-14 [&_>_svg]:w-14 flex justify-center py-4"
+            >
+              <CameraCaptureIcon />
+            </button>
+          </>
         )}
 
         {cameraCanBeUsed === false && (
@@ -103,6 +126,12 @@ export default function Home() {
       >
         <CameraToggleIcon />
       </button>
+      <div className="relative w-full flex justify-center">
+        <input type="file" accept="image/*" onChange={handleFileUpload} />
+        <div className="absolute w-full h-full top-0 bg-black pointer-events-none rounded-md [&>svg]:w-4 [&>svg]:h-4 flex justify-center gap-4 border-white border pt-1">
+          <UploadIcon />
+        </div>
+      </div>
     </main>
   );
 }
@@ -133,3 +162,27 @@ const CameraCaptureIcon = () => (
     />
   </svg>
 );
+
+const UploadIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    height="16"
+    width="16"
+    viewBox="0 0 512 512"
+  >
+    <path
+      opacity="1"
+      fill="#fff"
+      d="M288 109.3V352c0 17.7-14.3 32-32 32s-32-14.3-32-32V109.3l-73.4 73.4c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l128-128c12.5-12.5 32.8-12.5 45.3 0l128 128c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L288 109.3zM64 352H192c0 35.3 28.7 64 64 64s64-28.7 64-64H448c35.3 0 64 28.7 64 64v32c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V416c0-35.3 28.7-64 64-64zM432 456a24 24 0 1 0 0-48 24 24 0 1 0 0 48z"
+    />
+  </svg>
+);
+
+const convertToBase64 = (file: File): Promise<string> => {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result as string);
+    reader.onerror = (error) => reject(error);
+  });
+};
