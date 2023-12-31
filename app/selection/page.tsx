cab34ad5a2f14e5aa3bd7beadd8fdf999a15757e
Arjan Malda <95175291+arjanmalda@users.noreply.github.com>
2023-12-31 15:52:32 +0100

    Clean up selection page

--- app/selection/page.tsx
+++ app/selection/page.tsx
@@ -11,7 +11,6 @@ export default function CarouselPage() {
   const [photos, setPhotos] = useState<string[]>([]);
   const [selectedPhotos, setSelectedPhotos] = useState<string[]>([]);
   const [isLoading, setIsLoading] = useState(true);
-  const [showSelection, setShowSelection] = useState(false);
 
   useEffect(() => {
     setIsLoading(true);
@@ -59,136 +58,79 @@ export default function CarouselPage() {
       <PasswordProtection>
         {isLoading ? (
           <h1 className="text-4xl font-bold mb-4">Loading...</h1>
-        ) : showSelection ? undefined : (
+        ) : (
           <Fragment>
-            <h1 className="text-4xl font-bold mb-4 mt-10">All photos</h1>
-            <ul className="grid grid-cols-6 gap-2 mb-4">
-              {photos.map((photo, index) => (
-                <AnimatePresence key={photo.slice(-10)}>
-                  <motion.button
-                    key={photo.slice(-10)}
-                    onClick={() => addPhotoToSelection(photo)}
-                    className="h-72 overflow-hidden rounded-xl"
-                  >
-                    <Image
-                      className="-translate-y-1/3"
-                      src={photo}
-                      alt={`Photo ${index}`}
-                      width={1000}
-                      height={1000}
-                    />
-                  </motion.button>
-                </AnimatePresence>
-              ))}
-            </ul>
-            <h1 className="text-4xl font-bold mb-4">New selection</h1>
-            <ul className="grid grid-cols-6 gap-2">
-              {selectedPhotos.map((photo, index) => (
-                <AnimatePresence key={photo.slice(-10)}>
-                  <motion.button
-                    key={photo.slice(-10)}
-                    onClick={() => removePhotoFromSelection(photo)}
-                    className="h-72 overflow-hidden rounded-xl"
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    exit={{ opacity: 0 }}
-                  >
-                    <Image
-                      className="-translate-y-1/3"
-                      src={photo}
-                      alt={`Photo ${index}`}
-                      width={1000}
-                      height={1000}
-                    />
-                  </motion.button>
-                </AnimatePresence>
-              ))}
-            </ul>
-            <button
-              type="button"
-              onClick={saveSelectionToFirebase}
-              className={` cursor-pointer mt-8 p-2 rounded-md border border-white hover:bg-white hover:text-black ${
-                isLoading ? "opacity-50" : ""
-              }}`}
-              disabled={selectedPhotos.length === 0 || isLoading}
-            >
-              Save selection
-            </button>
+            {photos?.length > 0 && (
+              <Fragment>
+                <h1 className="text-4xl font-bold mb-4 mt-10">All photos</h1>
+                <ul className="grid grid-cols-6 gap-2 mb-4">
+                  {photos.map((photo, index) => (
+                    <AnimatePresence key={photo.slice(-10)}>
+                      <motion.button
+                        key={photo.slice(-10)}
+                        onClick={() => addPhotoToSelection(photo)}
+                        className="h-72 overflow-hidden rounded-xl"
+                      >
+                        <Image
+                          className="-translate-y-1/3"
+                          src={photo}
+                          alt={`Photo ${index}`}
+                          width={1000}
+                          height={1000}
+                        />
+                      </motion.button>
+                    </AnimatePresence>
+                  ))}
+                </ul>
+              </Fragment>
+            )}
+            {selectedPhotos?.length > 0 && (
+              <Fragment>
+                <h1 className="text-4xl font-bold mb-4">New selection</h1>
+                <ul className="grid grid-cols-6 gap-2">
+                  {selectedPhotos.map((photo, index) => (
+                    <AnimatePresence key={photo.slice(-10)}>
+                      <motion.button
+                        key={photo.slice(-10)}
+                        onClick={() => removePhotoFromSelection(photo)}
+                        className="h-72 overflow-hidden rounded-xl"
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        exit={{ opacity: 0 }}
+                      >
+                        <Image
+                          className="-translate-y-1/3"
+                          src={photo}
+                          alt={`Photo ${index}`}
+                          width={1000}
+                          height={1000}
+                        />
+                      </motion.button>
+                    </AnimatePresence>
+                  ))}
+                </ul>
+                <button
+                  type="button"
+                  onClick={saveSelectionToFirebase}
+                  className={` cursor-pointer mt-8 p-2 rounded-md border border-white hover:bg-white hover:text-black ${
+                    isLoading ? "opacity-50" : ""
+                  }}`}
+                  disabled={selectedPhotos.length === 0 || isLoading}
+                >
+                  Save selection
+                </button>
+              </Fragment>
+            )}
           </Fragment>
         )}
-        <button
-          className={` cursor-pointer mt-8 p-2 rounded-md border border-white hover:bg-white hover:text-black ${
-            isLoading ? "opacity-50" : ""
-          }}`}
-          onClick={() => setShowSelection((prev) => !prev)}
-        >
-          {showSelection ? "Hide" : "Show"} saved selection
-        </button>
         <AnimatePresence key={selectedPhotos[0]}>
           <motion.div
             initial={{ translateX: 1 }}
             animate={{ translateX: 0 }}
             exit={{ translateX: 1 }}
-          >
-            {!!showSelection && <SavedSelection />}
-          </motion.div>
+          ></motion.div>
         </AnimatePresence>
       </PasswordProtection>
     </main>
   );
 }
-
-const SavedSelection = () => {
-  const [photos, setPhotos] = useState<string[]>([]);
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    setIsLoading(true);
-    const unsubscribe = onSnapshot(collection(db, "selection"), (snapshot) => {
-      const selection = snapshot.docs.map((doc) => doc.data().photos);
-
-      setPhotos(selection[0]);
-    });
-    setIsLoading(false);
-
-    return () => unsubscribe();
-  }, []);
-
-  return (
-    <main
-      className="flex h-screen w-screen flex-col items-center justify-center p-8 bg-cover bg-center"
-      style={{ backgroundImage: `url('/photo-boot-background.jpeg')` }}
-    >
-      <PasswordProtection>
-        {isLoading ? (
-          <h1 className="text-4xl font-bold mb-4">Loading...</h1>
-        ) : (
-          <Fragment>
-            <h1 className="text-4xl font-bold mb-4">Opgeslagen selectie</h1>
-            <ul className="grid grid-cols-6 gap-2">
-              {photos?.map((photo, index) => (
-                <AnimatePresence key={photo.slice(-10)}>
-                  <motion.button
-                    key={photo.slice(-10)}
-                    className="h-72 overflow-hidden rounded-xl"
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    exit={{ opacity: 0 }}
-                  >
-                    <Image
-                      className="-translate-y-1/3"
-                      src={photo}
-                      alt={`Photo ${index}`}
-                      width={1000}
-                      height={1000}
-                    />
-                  </motion.button>
-                </AnimatePresence>
-              ))}
-            </ul>
-          </Fragment>
-        )}
-      </PasswordProtection>
-    </main>
-  );
-};
